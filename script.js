fe4fb6f801c1cd23c759e07f9bc08b088d1d1568
Frank Merchlewitz <jlimhb@gmail.com>
2021-01-26 12:36:55 -0600

    Successfully writing current city weather data to the page

--- script.js
+++ script.js
@@ -9,7 +9,14 @@
 // error handler
 
 // Ajax to pull API
+// set up Luxon's DateTime w/CDN
+var DateTime = luxon.DateTime;
 
+// Get the local current date and time
+var dt = DateTime.local();
+
+// store the current date as a string, displaying month, date, and year
+var currentDate = dt.toLocaleString(DateTime.DATE_MED);
 var cities = [];
 
 
@@ -26,10 +33,39 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&a
         var temp = Math.round(response.main.temp * (9/5) - 459.67) + "°F";
         var humidity = response.main.humidity + "%";
         var wind = Math.round(response.wind.speed) + " MPH";
+
+       $("#current-city").text(city + " weather for " + currentDate);
+
+       $("#temp").text("Temperature: " + temp);
+       $("#humidity").text("Humidity: " + humidity);
+       $("#wind").text("Wind: " + wind);
+       
+
+        
+        console.log(lon);
+        console.log(lat);
         
         console.log(temp);
         console.log(humidity);
         console.log(wind);
+
+
+
+        var lon = response.coord.lon;
+        var lat = response.coord.lat;
+
+        var queryURLUV = "https://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=" + APIKey;
+
+        $.ajax({
+            url: queryURLUV,
+            method: "GET"
+          }).then(function(response) {
+            console.log(response);
+            var UV = response.value.toFixed(1);
+            console.log(UV);
+            $("#UV").text("UV Index: " + UV);
+        });
+
       });
     
 });
