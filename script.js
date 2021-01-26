cba7c646a9e297f56f66653cb1e52e870aaa4c6a
Frank Merchlewitz <jlimhb@gmail.com>
2021-01-26 11:42:30 -0600

    Skeleton Built

---
+++ script.js
@@ -0,0 +1,12 @@
+// functions to write local storage and get local storage
+
+// function to handle search input and do a query of open weather
+
+// function to write 5 day forecast
+
+// function to write previously searched cities
+
+// error handler
+
+// Ajax to pull API
+
