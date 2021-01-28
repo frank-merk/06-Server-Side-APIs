d68cb0b26574d522cda87703fce01d58c15f7437
Frank Merchlewitz <jlimhb@gmail.com>
2021-01-28 15:46:43 -0600

    added error handling message

--- script.js
+++ script.js
@@ -22,11 +22,26 @@ var cities = [];
 
 $(".btn").on("click", function(event) {
     event.preventDefault();
+
     var city = $("#city").val().trim();
-    var APIKey = "fecb5e83c287868897c1ddcf3fb5404f";
-var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+    var APIKey = 
+    "fecb5e83c287868897c1ddcf3fb5404f";
+
+    var weatherQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+    var forecastQueryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+
     $.ajax({
-        url: queryURL,
+      url: queryURL,
+      method: "GET",
+      statusCode: {
+          404: function() {
+            alert("The city is mispelled or not in our weather database. Try another.");
+          }
+        }    
+
+    $.ajax({
+        url: weatherQueryURL,
         method: "GET"
       }).then(function(response) {
         console.log(response);
@@ -38,7 +53,7 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&a
         var iconSrc = "https://openweathermap.org/img/w/" + iconId + ".png";
         $(".icon").attr("src", iconSrc);
 
-       $("#current-city").text(city + " weather for " + currentDate);
+       $("#current-city").text(city + " weather for " + currentDate)  ;
 
        $("#temp").text("Temperature: " + temp);
        $("#humidity").text("Humidity: " + humidity);
@@ -64,5 +79,12 @@ var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&a
         });
 
       });
+
+      $.ajax({
+        url: forecastQueryURL,
+        method: "GET"
+      }).then(function(response) {
+        console.log(response);
+    });
     
 });
